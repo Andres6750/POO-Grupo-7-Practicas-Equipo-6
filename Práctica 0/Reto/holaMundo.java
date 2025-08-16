48aeb1695c50287c49acd2a56e5314b2a8c7c4ec
= <=>
2025-08-16 13:57:46 -0600

    Comentarios agregados

--- Práctica 0/Reto/holaMundo.java
+++ Práctica 0/Reto/holaMundo.java
@@ -1,7 +1,12 @@
 //Código fuente de un archivo Java que imprime "Hola Mundo"
-public class holaMundo{
-    public static void main (String[] args){
-        System.out.println("Hola mundo!");
-        System.out.println("Somos el equipo 6 :)");
+
+public class holaMundo{ //Clase holaMundo
+
+    public static void main (String[] args){  //Clase principal
+
+        System.out.println("Hola mundo!");   //Impresión de una linea
+        System.out.println("Somos el equipo 6 :)");    //Impresión de una linea
+
     }
+
 }
