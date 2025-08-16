11aa9a1988343d39ba52645e477ed6d3960fd6c3
AbdieeX <abdielbarush@gmail.com>
2025-08-16 15:59:34 -0600

    Espacios extra corregidos.

--- Práctica 0/Reto/holaMundo.java
+++ Práctica 0/Reto/holaMundo.java
@@ -1,12 +1,12 @@
 //Código fuente de un archivo Java que imprime "Hola Mundo"
+//Equipo 6
 
-public class holaMundo{ //Clase holaMundo
+public class holaMundo{ //Clase holaMundo (mismo nombre que el archivo .java)
 
-    public static void main (String[] args){  //Clase principal
+    public static void main (String[] args){  //Clase principal (punto de partida)
 
         System.out.println("Hola mundo!");   //Impresión de una linea
         System.out.println("Somos el Equipo 6 :)");    //Impresión de una linea
 
     }
-
 }
