d5cca54403ae008806363eda6e8dfc9e442d3c40
AbdieeX <abdielbarush@gmail.com>
2025-08-16 14:01:18 -0600

    Comentarios agregados 2

--- Práctica 0/Reto/holaMundo.java
+++ Práctica 0/Reto/holaMundo.java
@@ -5,7 +5,7 @@ public class holaMundo{ //Clase holaMundo
     public static void main (String[] args){  //Clase principal
 
         System.out.println("Hola mundo!");   //Impresión de una linea
-        System.out.println("Somos el equipo 6 :)");    //Impresión de una linea
+        System.out.println("Somos el Equipo 6 :)");    //Impresión de una linea
 
     }
 
