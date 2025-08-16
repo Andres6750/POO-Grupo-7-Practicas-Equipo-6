f21043b85d872bc2ad9695757985f0b78281980a
= <=>
2025-08-16 13:55:24 -0600

    Código "finalizado"

--- Práctica 0/Reto/holaMundo.java
+++ Práctica 0/Reto/holaMundo.java
@@ -1,1 +1,7 @@
 //Código fuente de un archivo Java que imprime "Hola Mundo"
+public class holaMundo{
+    public static void main (String[] args){
+        System.out.println("Hola mundo!");
+        System.out.println("Somos el equipo 6 :)");
+    }
+}
