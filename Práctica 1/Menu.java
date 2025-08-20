790d0989c17e5effdcd16ec2ecbff69f754aca3f
Valdo48 <oswasamatos@gmail.com>
2025-08-20 14:02:47 -0600

    Funciona sin metodos de String

--- Práctica 1/Menu.java
+++ Práctica 1/Menu.java
@@ -21,25 +21,26 @@ public class Menu{
 
                 switch (option) { // Menú de opciones
                     case 1:
-                        System.out.println("Ingrese el número que desea procesar");
+                        System.out.println("Ingrese el número del que desea conocer el factorial:");
                         n = inp.nextInt();
                         System.out.println("El factorial de "+ n +" es: "+ FactorialR(n));
                         break;
                     case 2:
-                        System.out.println("Ingrese el número que desea procesar");
+                        System.out.println("Ingrese el número de terminos de la sucesión de Fibonacci:");
                         n = inp.nextInt();
                         System.out.println("La sucesión de Fibonacci hasta "+ n +" es:");
-                        System.out.println(Fibonacci(n));
+                        Fibonacci(n);
                         break;
                     case 3:
-                        System.out.println("Ingrese el número que desea procesar");
+                        System.out.println("Ingrese el número desde que iniciara la sucesión:");
                         n = inp.nextInt();
                         System.out.println("La sucesión generada es la siguiente:");
-                        System.out.println(Collatz(n));
+                        Collatz(n);
                         break;               
                     default:
                         break;
                 }
+                System.out.println("");
             }
 
         } while (option != 4);  
@@ -53,28 +54,27 @@ public class Menu{
         }
     }
 
-    public static String Fibonacci(int n) { //Método para la serie de Fibonacci
+    public static void Fibonacci(int n) { //Método para la serie de Fibonacci
         int ant = 0, fib = 1;
-        String serie = "1"; // Declaro una cadena para mostrar la sucesión de Fibonacci
         for(int i = 0; i < n; i++) {
+            System.out.print(fib + "->");
             int aux = fib;
             fib += ant;
             ant = aux;
-            serie = serie + "->" + String.valueOf(fib); // Agrega el término a la sucesión de retorno 
         }
-        return serie;   
+        System.out.println("Fin."); // Indica que la sucesión termino
+  
     }
 
-    public static String Collatz(int n) { //Método para la conjetura de Collatz
-        String collatz = String.valueOf(n);
-        while(n != 1){ //Verifica que la sucesión no entre en un bucle infinito 
+    public static void Collatz(int n) { //Método para la conjetura de Collatz
+        while(n != 1){
+            System.out.print(n + "->"); //Verifica que la sucesión no entre en un bucle infinito 
             if(n%2 == 0) { //Condición para pares
                 n /= 2;
             }else { // Condición para impares
                 n = 3*n + 1;
             }       
-            collatz = collatz + "->" + String.valueOf(n); // Agrega el término a la sucesión de retorno  
         }
-        return collatz;
+        System.out.print(1); //Imprime el número donde termina la sucesión
     }
 }
