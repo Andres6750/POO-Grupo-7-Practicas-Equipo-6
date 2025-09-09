b7334b12560b3f8ac7529e2d742229699f8e174e
Andres6750 <andresgf2006@gmail.com>
2025-09-08 19:11:34 -0600

    Primer intento Práctica 4

--- Práctica 4/Practica4.java
+++ Práctica 4/Practica4.java
@@ -1,9 +1,12 @@
 import javax.swing.*;
 import java.awt.event.*;
+import java.math.*;
+import java.util.Scanner;
 
 class Mensajes {
+    public double distanciaPuntos;
     public String mensaje() {
-        return "Hola! tengo vida! c:";
+        return "La distancia entre los vectores es: " + distanciaPuntos + ".";
     }   
 }
 
@@ -14,12 +17,12 @@ class Ventana extends JFrame {
     public Ventana(Mensajes controlador) {
         this.controlador = controlador;
 
-        setTitle("Ejemplo");
+        setTitle("Distancia entre puntos");
         setSize(300, 200);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
-        boton = new JButton("Haz clic aqui");
+        boton = new JButton("Haz clic aqui para saber la distancia entre los puntos");
         boton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -34,9 +37,24 @@ class Ventana extends JFrame {
 
 public class Practica4 {
     public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Ingresa el primer componente del primer vector (x)");
+        double x1 = inp.nextDouble();
+        System.out.println("Ingresa el segundo componente del primer vector (y)");
+        double y1 = inp.nextDouble();
+        System.out.println("Ingresa el primer componente del segundo vector (x)");
+        double x2 = inp.nextDouble();
+        System.out.println("Ingresa el segundo componente del segundo vector (y)");
+        double y2 = inp.nextDouble();
         Mensajes controlador = new Mensajes();
+        double distanciaPuntos = distancia(x1, y1, x2, y2);
 
         Ventana ventana = new Ventana(controlador);
         ventana.setVisible(true);
+    }
+
+    public static double distancia (double x1 , double y1 , double x2 , double y2){
+        double distanciaPuntos = Math.hypot(x2 - x1, y2 - y1);
+        return distanciaPuntos;
     }
 }
