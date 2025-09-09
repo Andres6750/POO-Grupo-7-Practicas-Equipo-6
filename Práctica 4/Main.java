0738c12f3771864e3e0c43334e437d9aa1e041ec
Andrés Guzmán Fernández <andresgf2006@gmail.com>
2025-09-08 18:00:34 -0600

    Create Main.java

---
+++ Práctica 4/Main.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Mensajes {
+    public String mensaje() {
+        return "Hola! tengo vida! c:";
+    }   
+}
+
+public class Ventana extends JFrame {
+    JButton boton;
+    Mensajes controlador;
+
+    public Ventana(Mensajes controlador) {
+        this.controlador = controlador;
+
+        setTitle("Ejemplo");
+        setSize(300, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        boton = new JButton("Haz clic aqui");
+        boton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String mensaje = controlador.mensaje();
+                JOptionPane.showMessageDialog(null, mensaje);
+            }
+        });
+
+        add(boton);
+    }
+}
+
+public class Practica4 {
+    public static void main(String[] args) {
+        Mensajes controlador = new Mensajes();
+
+        Ventana ventana = new Ventana(controlador);
+        ventana.setVisible(true);
+    }
+}
