409ba8fc9a0340edb752ecfc38cc93276bf1a167
Abdiel Barush Martínez Ruiz <140469696+AbdieeX@users.noreply.github.com>
2025-09-12 21:08:15 -0600

    Códigos de la pŕactica 4

--- Práctica 4/Practica4.java
+++ Práctica 4/Practica4.java
@@ -1,1 +1,19 @@
-//Hola pon el código jejej
+public class Practica4{
+    public static void main (String[] args){
+        Punto p1 = new Punto(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+        Punto p2 = new Punto(Integer.parseInt(args[2]), Integer.parseInt(args[3]));
+
+        double distanciaPuntos = Math.hypot(p2.x - p1.x, p2.y - p1.y);
+
+        Mensajes controlador = new Mensajes();
+        controlador.distanciaPuntos = distanciaPuntos;
+        controlador.x1 = p1.x;
+        controlador.x2 = p1.y;
+        controlador.y1 = p2.x;
+        controlador.y2 = p2.y;
+
+        Ventana ventana = new Ventana(controlador);
+        ventana.setVisible(true);
+    }
+
+}
